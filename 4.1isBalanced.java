979989811f3cf47e43fac51d333c21b55f710433
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-05-14 12:09:54 -0400

    Added test case

--- 4.1isBalanced.java
+++ 4.1isBalanced.java
@@ -1,6 +1,7 @@
 /*Implement a function to check if a binary tree is balanced. for the purpose
 of this question, a balanced tree is defined to be a tree such that the heights
 of the two subtrees of any node never differ by more than one.*/
+
 class TreeNode{
 	Object data;
 	TreeNode leftChild;
@@ -9,39 +10,25 @@ class TreeNode{
 		data=input;
 	}
 
-	static boolean isBalanced(TreeNode node){
+	public static boolean isBalanced(TreeNode node){
 		boolean result;
 		if(node==null){
 			return true;
 		}
-		if (node.leftChild==null && node.rightChild==null)
-			return true;
-		if (node.leftChild==null){
-			if (node.rightChild!=null && node.rightChild.leftChild==null 
-				&& node.rightChild.rightChild==null)
-				return true;
-			else
-				return false;
-		}
 
-		if (node.rightChild==null){
-
-			if (node.leftChild!=null && node.leftChild.leftChild==null 
-				&& node.leftChild.rightChild==null)
-				return true;
-			else
-				return false;
-		}
 		else{
+			result=Math.abs(Height(node.leftChild)-Height(node.rightChild))<=1;
+			if (!result) 
+				return false;
 			return isBalanced(node.leftChild) && isBalanced (node.rightChild);
 		}
 
 	}
 
-	static int Height (TreeNode node){
+	public static int Height (TreeNode node){
 		int height;
 		if (node==null){
-			return 0;
+			return -1;
 		}
 		else{
 			height=max(Height(node.leftChild),Height(node.rightChild))+1;
@@ -65,6 +52,24 @@ class Tree {
 	public Tree(Object input){
 		root=new TreeNode(input);
 	}
+}
 
-
+class test{
+	public static void main(String[] args){
+		Tree tree=new Tree(1);
+		tree.root.leftChild=new TreeNode(2);
+		tree.root.leftChild.rightChild=new TreeNode(2);
+		tree.root.rightChild=new TreeNode(3);
+		tree.root.rightChild.leftChild=new TreeNode(3);
+		tree.root.rightChild.rightChild=new TreeNode(3);
+		tree.root.rightChild.rightChild.rightChild=new TreeNode(3);
+		tree.root.leftChild.leftChild=new TreeNode(2);
+		tree.root.leftChild.leftChild.leftChild=new TreeNode(2);
+		int height=TreeNode.Height(tree.root);
+		System.out.println("The height of the tree is "+ height);
+		if (TreeNode.isBalanced(tree.root))
+			System.out.println("I'm balanced");
+		else
+			System.out.println("I'm not a balanced tree");
+	}
 }
