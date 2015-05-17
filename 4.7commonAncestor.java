7c69533372fd596d40019edd3379840b5c0d5279
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-05-17 17:21:01 -0400

    Find out the common ancestor

--- 4.7commonAncestor.java
+++ 4.7commonAncestor.java
@@ -8,14 +8,56 @@ class TreeNode{
 	public TreeNode(int input){
 		item=input;
 	}
-	public TreeNode commonAncestor(TreeNode root,TreeNode node1,TreeNode node2){
+	static TreeNode commonAncestor(TreeNode root,TreeNode node1,TreeNode node2){
+		if (node1==root ||node2==root)
+			return null;
+		while (true){
+			if (isChild(root.leftChild,node1) && isChild(root.leftChild,node2))
+				root=root.leftChild;
+			else if (isChild(root.rightChild,node1) && isChild(root.rightChild,node2))
+				root=root.rightChild;
+			else
+				return root;
+		}
+	}
 
-
+	static boolean isChild(TreeNode root, TreeNode node){
+		if (root==null)
+			return false;
+		if (root.leftChild==node || root.rightChild==node)
+			return true;
+		else
+			return isChild(root.leftChild,node) || isChild(root.rightChild,node);
 	}
 }
-class Tree{
-	TreeNode root;
-	public Tree(int input){
-		root=new TreeNode(input);
+
+
+class test{
+	public static void main(String[] args){
+		
+		TreeNode root=new TreeNode(0);
+		TreeNode node1=new TreeNode(1);
+		TreeNode node2=new TreeNode(2);
+		TreeNode node3=new TreeNode(3);
+		TreeNode node4=new TreeNode(4);
+		TreeNode node5=new TreeNode(5);
+		TreeNode node6=new TreeNode(6);
+		TreeNode node7=new TreeNode(7);
+		TreeNode node8=new TreeNode(8);
+		root.leftChild=node1;
+		root.rightChild=node2;
+		node1.leftChild=node3;
+		node1.rightChild=node4;
+		node4.rightChild=node5;
+		node5.rightChild=node6;
+		node2.leftChild=node7;
+		node2.rightChild=node8;
+		TreeNode result=TreeNode.commonAncestor(root,node3,node6);
+		if (result==null)
+			System.out.println("No common ancestor");
+		else
+			System.out.println(result.item);
+		
+		
 	}
 }
