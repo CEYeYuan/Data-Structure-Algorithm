cf03b922fe3a5121c281aeb2caba1b2d76657b29
CEYeYuan <you@example.com>
2015-06-02 11:00:42 -0400

    Fixed 5.8

--- 5.8drawLine.java
+++ 5.8drawLine.java
@@ -3,7 +3,11 @@ consecutive pixels to be stored in one byte. The screen has width w, where w
 is divisible by 8 (that is, no byte will be split across rows). The height of
 the screen, of course, can be derived from the length of the array and the 
 width. Implement a function drawHorizontalLine(byte[] screen, int width, int x1,
-int x2,int y) which draws a horizontal line from (x1,y) to (x2,y).*/
+int x2,int y) which draws a horizontal line from (x1,y) to (x2,y).
+####
+By default, all bitwise operation return an integer, so we need to cast explicitly
+to get a byte back
+*/
 class test{
 	public static byte[] drawHorizontalLine(byte[] screen,int width,int x1,int x2,int y){
 		//x1, x2, y all starts from 1;x1, x2 inclusive
@@ -35,16 +39,16 @@ class test{
 	public static void setBit(byte[] input,int index){
 		int quotient=index/8;
 		int remainder=index%8;
-		input[quotient]=input[quotient]|((byte) (1<<remainder));
+		input[quotient]=(byte)(input[quotient]|(1<<remainder));
 	}
 	public static void main (String[] args){
 		int width=8*2;
 		int height=10;
 		byte[] screen=new byte[width*height/8];
 		//screen[1]=(byte)0b11111111;
-		int x1=1;
-		int x2=10;
-		int y=5;
+		int x1=2;
+		int x2=3;
+		int y=1;
 		screen=drawHorizontalLine(screen,width,x1,x2,y);
 		drawScreen(screen,width);
 	}
