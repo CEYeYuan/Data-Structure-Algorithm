16fbf374eb104d83bfc4bc8579775d1938090a6a
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-10-04 20:39:17 -0400

    iterative

--- leetcode/BinaryTreeInorderTraversal.java
+++ leetcode/BinaryTreeInorderTraversal.java
@@ -12,3 +12,50 @@ return [1,3,2].
 
 Note: Recursive solution is trivial, could you do it iteratively?
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<Integer>();
+        traversal(list,root);
+        return list;
+    }
+    private void traversal(List<Integer> list,TreeNode root){
+        if(root==null)  return;
+        if(root.left!=null){
+            traversal(list,root.left);
+        }
+        list.add(root.val);
+        if(root.right!=null){
+              traversal(list,root.right);
+        }
+    }
+}
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<Integer>();
+        Stack<TreeNode> stack=new Stack<TreeNode>();
+        if(root==null)  return list;
+        
+        while(root!=null||!stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root=root.left;
+            }
+            root=stack.pop();
+            list.add(root.val);//visit it self
+            root=root.right;//recursively visit its right child
+            
+        }
+        return list;
+    }
+}
